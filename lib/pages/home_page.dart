072ac5644e09ecf9ebe01b0f3e6a2fb316231163
Mateus Reis <mateus.reis@com3telecom.com.br>
2026-01-14 17:02:31 -0300

    feat: fill the home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:habits_app/theme/theme_provider.dart';
+import 'package:habits_app/components/my_drawer.dart';
+import 'package:habits_app/components/my_habit_tile.dart';
+import 'package:habits_app/database/habit_database.dart';
+import 'package:habits_app/models/habit.dart';
+import 'package:habits_app/utils/habit_util.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,30 +14,80 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _textController = TextEditingController();
+
+  // create new habit
+  void createNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("New Habit"),
+        content: TextField(
+          controller: _textController,
+          decoration: InputDecoration(
+            labelText: "Name",
+            border: OutlineInputBorder(),
+          ),
+        ),
+        actions: [
+          MaterialButton(
+            color: Theme.of(context).colorScheme.primary,
+            onPressed: () {
+              // create
+              context.read<HabitDatabase>().addHabit(_textController.text);
+              // clear controller
+              _textController.clear();
+              // popout dialog
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Save",
+              style: TextStyle(color: Theme.of(context).colorScheme.surface),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void checkHabit(bool? value, Habit habit) {
+    if (value != null) {
+      context.read<HabitDatabase>().updateHabitCompletion(habit.id, value);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            DrawerHeader(child: Text("Habits App")),
-            Row(
-              children: [
-                Text("Dark Mode"),
-                Switch(
-                  value: Provider.of<ThemeProvider>(context).isDarkMode,
-                  onChanged: (value) => Provider.of<ThemeProvider>(
-                    context,
-                    listen: false,
-                  ).toggleTheme(),
-                ),
-              ],
-            ),
-          ],
-        ),
+      drawer: MyDrawer(),
+      body: _buildHabitList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        child: Icon(Icons.add),
       ),
-      body: Center(child: Text("Habits App!")),
+    );
+  }
+
+  Widget _buildHabitList() {
+    // habits db
+    final habitDatabase = context.watch<HabitDatabase>();
+
+    // list of habits
+    List<Habit> currentHabits = habitDatabase.currentHabits;
+
+    return ListView.builder(
+      itemCount: currentHabits.length,
+      itemBuilder: (context, index) {
+        final habit = currentHabits[index];
+        bool isCompletedToday = isHabitCompletedToday(habit.completedDays);
+
+        return MyHabitTile(
+          habit: habit,
+          isCompleted: isCompletedToday,
+          onChanged: (value) => checkHabit(value, habit),
+        );
+      },
     );
   }
 }
