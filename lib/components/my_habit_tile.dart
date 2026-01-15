004d061474443800ee38f5fe9d91ee23593bcafd
Mateus Reis <mateus.reis@com3telecom.com.br>
2026-01-15 11:02:36 -0300

    feat: create habit tile

---
+++ lib/components/my_habit_tile.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:habits_app/models/habit.dart';
+
+class MyHabitTile extends StatelessWidget {
+  final Habit habit;
+  final bool isCompleted;
+  final void Function(bool?)? onChanged;
+  final void Function(BuildContext)? editHabit;
+  final void Function(BuildContext)? deleteHabit;
+
+  const MyHabitTile({
+    super.key,
+    required this.habit,
+    required this.isCompleted,
+    required this.onChanged,
+    required this.editHabit,
+    required this.deleteHabit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: editHabit,
+              icon: Icons.edit,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            SlidableAction(
+              onPressed: deleteHabit,
+              icon: Icons.delete,
+              backgroundColor: Colors.red,
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: isCompleted
+                ? Colors.green
+                : Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            onTap: () {
+              if (onChanged != null) {
+                onChanged!(!isCompleted);
+              }
+            },
+            leading: Checkbox(value: isCompleted, onChanged: onChanged),
+            title: Text(
+              habit.name,
+              style: TextStyle(
+                color: isCompleted
+                    ? Theme.of(context).colorScheme.tertiary
+                    : Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
