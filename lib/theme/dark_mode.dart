4d78c82e777416767dfd985de9c1999b7e8ec377
Mateus Reis <mateus.reis@com3telecom.com.br>
2026-01-15 12:49:22 -0300

    fix: dark mode inverse primary color

--- lib/theme/dark_mode.dart
+++ lib/theme/dark_mode.dart
@@ -6,6 +6,6 @@ ThemeData darkMode = ThemeData(
     primary: Colors.blueGrey,
     secondary: Colors.blueGrey.shade700,
     tertiary: Colors.blueGrey.shade900,
-    inversePrimary: Colors.blueGrey.shade800,
+    inversePrimary: Colors.blueGrey.shade100,
   ),
 );
